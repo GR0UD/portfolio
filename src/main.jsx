3b47d52b7239bde6f077d48151782ce7e00e7aee
GROUD <marksgalkins12345678910@gmail.com>
2025-10-27 19:52:10 +0100

    B

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './styles/main.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./styles/main.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Remove loading state after React hydration
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.removeAttribute("unresolved");
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
